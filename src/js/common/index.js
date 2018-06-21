d8a4568e13fcb360bb3ed346d24e000fc7b79130
liudewei <3151081884@qq.com>
2018-06-21 15:10:16 +0800

    demolink page

--- src/js/common/index.js
+++ src/js/common/index.js
@@ -1,17 +1,21 @@
-import Lazyload from './lazyload.js'
-import responsive from './responsive.js'
-import handleNavs from './nav.js'
-import { getId, showForm, returnToBack, onInitScorllListener } from './utils'
+import Lazyload from './lazyload.js';
+import responsive from './responsive.js';
+import handleNavs from './nav.js';
+import {getId, onInitScorllListener, returnToBack, showForm, showDemoLink} from './utils';
 
 responsive().then(() => {
-  getId('loading').style.display = 'none'
-  const $backToTop = getId('backToTop')
-  const $header = getId('gHeader')
-  const $formLink = getId("formLink")
-  showForm($formLink)
-  returnToBack($backToTop)
-  handleNavs()
-  onInitScorllListener($backToTop, $header)
+  getId('loading').style.display = 'none';
+  const $backToTop = getId('backToTop');
+  const $header = getId('gHeader');
+  const $formLink = getId('formLink');
+  const $demoLink = getId('demoLink');
+  const $demoHeader = getId('demoHeader');
+  showForm($formLink);
+  showDemoLink($demoHeader);
+  showDemoLink($demoLink);
+  returnToBack($backToTop);
+  handleNavs();
+  onInitScorllListener($backToTop, $header);
   /* eslint-disable no-new */
-  new Lazyload()
-})
+  new Lazyload();
+});
--- src/js/common/utils.js
+++ src/js/common/utils.js
@@ -1,127 +1,148 @@
-import { physics } from 'popmotion'
-import scroll from 'stylefire/scroll'
-import throttle from 'lodash/throttle'
+import {physics} from 'popmotion';
+import scroll from 'stylefire/scroll';
+import throttle from 'lodash/throttle';
 
 function hasClass (elem, cls) {
-  cls = cls || ''
+  cls = cls || '';
   if (cls.replace(/\s/g, '').length == 0) {
-    return false
+    return false;
   } // 当cls没有参数时，返回false
-  return new RegExp(' ' + cls + ' ').test(' ' + elem.className + ' ')
+  return new RegExp(' ' + cls + ' ').test(' ' + elem.className + ' ');
 }
 
 function addClass (elem, cls) {
   if (!hasClass(elem, cls)) {
-    elem.className = elem.className == '' ? cls : elem.className + ' ' + cls
+    elem.className = elem.className == '' ? cls : elem.className + ' ' + cls;
   }
 }
 
 function removeClass (elem, cls) {
   if (hasClass(elem, cls)) {
-    var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, '') + ' '
+    var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, '') + ' ';
     while (newClass.indexOf(' ' + cls + ' ') >= 0) {
-      newClass = newClass.replace(' ' + cls + ' ', ' ')
+      newClass = newClass.replace(' ' + cls + ' ', ' ');
     }
-    elem.className = newClass.replace(/^\s+|\s+$/g, '')
-  }
-}
-function _getScrollInfo () {
-  const windowScroll = scroll()
-  const top = windowScroll.get('top')
-  const winHeight =
-    document.documentElement.clientHeight || document.body.clientHeight
-  return {
-    windowScroll,
-    top,
-    winHeight
+    elem.className = newClass.replace(/^\s+|\s+$/g, '');
   }
 }
 
-function _setReturnBackClassName ($backToTop) {
-  const { top, winHeight } = _getScrollInfo()
-  $backToTop.className =
-    top > winHeight ? 'back-to-top show' : 'back-to-top hide'
+function _getScrollInfo () {
+  const windowScroll = scroll();
+  const top = windowScroll.get('top');
+  const winHeight =
+    document.documentElement.clientHeight || document.body.clientHeight;
+  return {
+    windowScroll,
+    top,
+    winHeight,
+  };
 }
+
+function _setReturnBackClassName ($backToTop) {
+  const {top, winHeight} = _getScrollInfo();
+  $backToTop.className =
+    top > winHeight ? 'back-to-top show' : 'back-to-top hide';
+}
+
 function _setHeaderStyle ($header) {
-  const { top } = _getScrollInfo()
-  const CLASS = 'header-with-color'
+  const {top} = _getScrollInfo();
+  const CLASS = 'header-with-color';
   if (top > 0) {
-    addClass($header, CLASS)
+    addClass($header, CLASS);
   } else if (hasClass($header, CLASS)) {
-    removeClass($header, CLASS)
+    removeClass($header, CLASS);
   }
 }
 
 function _scrollTop (from, to) {
-  const { windowScroll } = _getScrollInfo()
+  const {windowScroll} = _getScrollInfo();
   physics({
     from,
     to,
     springStrength: 500,
-    friction: 1
-  }).start(v => windowScroll.set('top', v))
+    friction: 1,
+  }).start(v => windowScroll.set('top', v));
 }
 
-export const ua = navigator.userAgent
+export const ua = navigator.userAgent;
 
 /**
  * @param {string} id
  * @returns element
  */
 export function getId (id) {
-  return document.getElementById(id)
+  return document.getElementById(id);
 }
 
 /**
  * @returns boolean
  */
 export function isMobile () {
-  return /Android|webOS|iPhone|iPod|BlackBerry/i.test(ua)
+  return /Android|webOS|iPhone|iPod|BlackBerry/i.test(ua);
 }
 
 export function navScroll (
   $navItems,
   $header,
   initClass = 'i-nav-main-item',
-  activeClass = 'i-nav-main-item-active'
+  activeClass = 'i-nav-main-item-active',
 ) {
   Array.from($navItems).forEach(item => {
     item && item.addEventListener('click', function () {
-      const { windowScroll, top } = _getScrollInfo()
-      const $node = getId(this.getAttribute('data-target'))
-      const currentTop = $node.offsetTop - $header.offsetHeight
+      const {windowScroll, top} = _getScrollInfo();
+      const $node = getId(this.getAttribute('data-target'));
+      const currentTop = $node.offsetTop - $header.offsetHeight;
       Array.from($navItems).forEach(item => {
-        item.className = initClass
-      })
-      this.className = initClass + ' ' + activeClass
-      _scrollTop(top, currentTop)
-    })
-  })
+        item.className = initClass;
+      });
+      this.className = initClass + ' ' + activeClass;
+      _scrollTop(top, currentTop);
+    });
+  });
 }
 
 export function showForm ($formLink) {
   $formLink && $formLink.addEventListener('click', function () {
-    $formLink.style.display = 'none'
-    getId('mc_embed_signup').style.display = 'block'
-  })
+    $formLink.style.display = 'none';
+    getId('mc_embed_signup').style.display = 'block';
+  });
 }
+
+export function showDemoLink ($demoLink) {
+  $demoLink && $demoLink.addEventListener('click', function () {
+    let $demoLayer = getId('demoLayer');
+    if ($demoLayer) {
+      $demoLayer.style.visibility = 'visible';
+      document.body.style.overflow = 'hidden';
+      $demoLayer.addEventListener('click', function (event) {
+        if(event.target.className && event.target.className.indexOf("i-demo-inner") > -1) {
+          return false;
+        } else {
+          $demoLayer.style.visibility = 'hidden';
+          document.body.style.overflow = 'auto';
+        }
+      });
+    }
+  });
+}
+
 export function returnToBack ($backToTop) {
   $backToTop && $backToTop.addEventListener('click', function () {
     const scrollTop =
-      document.documentElement.scrollTop || document.body.scrollTop
-    _scrollTop(scrollTop, 0)
-  })
+      document.documentElement.scrollTop || document.body.scrollTop;
+    _scrollTop(scrollTop, 0);
+  });
 }
 
 // 事件监听
 export function onInitScorllListener ($backToTop, $header) {
-  _setReturnBackClassName($backToTop)
+  _setReturnBackClassName($backToTop);
   window.addEventListener(
     'scroll',
     throttle(function () {
-      _setHeaderStyle($header)
-      _setReturnBackClassName($backToTop)
-    }, 300)
-  )
+      _setHeaderStyle($header);
+      _setReturnBackClassName($backToTop);
+    }, 300),
+  );
 }
 
